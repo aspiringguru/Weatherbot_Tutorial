dd74557d404f8e570a1c0f1e1e69b90d7a402b22
aspiringguru <bmatthewtaylor@gmail.com>
2019-02-23 13:46:58 +1000

    simple version of stories.md created 2

--- Full Code [Latest release of Rasa NLU and Rasa Core]/train_interactive.py
+++ Full Code [Latest release of Rasa NLU and Rasa Core]/train_interactive.py
@@ -18,13 +18,15 @@ logger = logging.getLogger(__name__)
 def run_weather_online(interpreter,
                           domain_file="weather_domain.yml",
                           training_data_file='data/stories.md'):
-    action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")						  
+    #action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
+    action_endpoint = EndpointConfig(url="http://0.0.0.0:5055/webhook")
     agent = Agent(domain_file,
-                  policies=[MemoizationPolicy(max_history=2), KerasPolicy(max_history=3, epochs=3, batch_size=50)],
+                  policies=[MemoizationPolicy(max_history=2),
+                            KerasPolicy(max_history=3, epochs=3, batch_size=50)],
                   interpreter=interpreter,
 				  action_endpoint=action_endpoint)
-    				  
-    data = agent.load_data(training_data_file)			   
+
+    data = agent.load_data(training_data_file)
     agent.train(data)
     interactive.run_interactive_learning(agent, training_data_file)
     return agent
